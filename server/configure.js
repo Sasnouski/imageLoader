dae16a8f3b17ea54b61805c703245ff21544f927
Artsiom Sasnouski <asasnouski@exadel.com>
2015-12-15 15:07:21 +0300

    Added views

--- server/configure.js
+++ server/configure.js
@@ -1,32 +1,37 @@
 
 
-var connect = require('connect'),
-    path = require('path'),
+var path = require('path'),
     routes = require('./routes'),
-    exphbs = require('express-handlebars');
-
+    exphbs = require('express3-handlebars'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    morgan = require('morgan'),
+    methodOverride = require('method-override'),
+    errorHandler = require('errorhandler'),
+    moment = require('moment');
 module.exports = function(app) {
-
     app.engine('handlebars', exphbs.create({
         defaultLayout: 'main',
         layoutsDir: app.get('views') + '/layouts',
-        partialsDir: [app.get('views') + '/partials']
+        partialsDir: [app.get('views') + '/partials'],
+        helpers: {
+            timeago: function(timestamp) {
+                return moment(timestamp).startOf('minute').fromNow();
+            }
+        }
     }).engine);
     app.set('view engine', 'handlebars');
-
-    app.use(connect.logger('dev'));
-    app.use(connect.bodyParser({
+    app.use(morgan('dev'));
+    app.use(bodyParser({
         uploadDir:path.join(__dirname, '../public/upload/temp')
     }));
-    app.use(connect.json());
-    app.use(connect.urlencoded());
-    app.use(connect.methodOverride());
-    app.use(connect.cookieParser('some-secret-value-here'));
-    app.use(app.router);
-    app.use('/public/', connect.static(path.join(__dirname, '../public')));
+    app.use(methodOverride());
+    app.use(cookieParser('some-secret-value-here'));
+    routes.initialize(app, new express.Router());
+    app.use('/public/', express.static(path.join(__dirname, '../public')));
     if ('development' === app.get('env')) {
-        app.use(connect.errorHandler());
+        app.use(errorHandler());
     }
-    routes.initialize(app);
     return app;
 };
